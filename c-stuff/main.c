b7e3b7499465f75f6de19a8fca7f5eb6112aaa67
leucht <lucas.philipp@st.ovgu.de>
2025-02-07 18:53:38 +0100

    Small changes

--- c-stuff/main.c
+++ c-stuff/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <netcdf.h>
 #include <time.h>
 #include <hdf5.h>
@@ -207,26 +208,137 @@ void bench_hdf5_read()
     printf("Finished saving \n");
 }
 
-/* int test_open()
+void create_hdf5_subfiling(int argc, char **argv)
 {
-    int status = NC_NOERR;
-    int ncid;
-    return status = nc_open("file:///home/test/dkrz_dev/data/test_dataset.zarr#mode=nczarr,file", NC_WRITE, &ncid);
+    H5FD_subfiling_config_t subf_config;
+    H5FD_ioc_config_t ioc_config;
+    MPI_Comm comm = MPI_COMM_WORLD;
+    MPI_Info info = MPI_INFO_NULL;
+    hid_t file_id = H5I_INVALID_HID;
+    hid_t fapl_id = H5I_INVALID_HID;
+    int mpi_thread_required = MPI_THREAD_MULTIPLE;
+    int mpi_thread_provided = 0;
+    /* Initialize MPI with threading support */
+    MPI_Init_thread(&argc, &argv, mpi_thread_required, &mpi_thread_provided);
+
+    if (mpi_thread_provided != mpi_thread_required)
+        MPI_Abort(comm, -1);
+    /*
+     * Set up File Access Property List with MPI
+     * parameters for the Subfiling VFD to use
+     */
+    fapl_id = H5Pcreate(H5P_FILE_ACCESS);
+    H5Pset_mpi_params(fapl_id, comm, info);
+    /*
+     * Get a default Subfiling and IOC configuration
+     */
+    H5Pget_fapl_subfiling(fapl_id, &subf_config);
+    H5Pget_fapl_ioc(fapl_id, &ioc_config);
+    /*
+     * Set Subfiling configuration to use a 1MiB
+     * stripe size.
+     */
+    // subf_config.shared_cfg.stripe_size = 1048576;
+    /*
+     * Set IOC configuration to use 2 worker threads
+     * per IOC instead of the default setting.
+     */
+    // ioc_config.thread_pool_size = 2;
+
+    /*
+     * Set our new configuration on the IOC
+     * FAPL used for Subfiling
+     */
+    H5Pset_fapl_ioc(subf_config.ioc_fapl_id, &ioc_config);
+    /*
+     * Finally, set our new Subfiling configuration
+     * on the original FAPL
+     */
+    H5Pset_fapl_subfiling(fapl_id, &subf_config);
+    /*
+     * Create a new Subfiling-based HDF5 file
+     */
+    file_id = H5Fcreate("data/datasets/test_dataset_subfiling.h5", H5F_ACC_TRUNC, H5P_DEFAULT, fapl_id);
+
+
+    hid_t dset, fspace, lcpl;
+
+    lcpl = H5Pcreate(H5P_LINK_CREATE);
+
+    fspace = H5Screate_simple(3, (hsize_t[]){512}, NULL);
+
+    dset = H5Dcreate2(file_id, "/X", H5T_IEEE_F64LE, fspace, lcpl, H5P_DEFAULT, H5P_DEFAULT);
+
+    /* Close/free resources */
+
+    H5Dclose(dset);
+    H5Sclose(fspace);
+    H5Pclose(lcpl);
+
+    H5Pclose(fapl_id);
+    H5Fclose(file_id);
+    MPI_Finalize();
 }
 
-int test_open_netcdf()
-{
-    int status = NC_NOERR;
-    int ncid;
-    return status = nc_open("data/test_dataset.nc", NC_WRITE, &ncid);
-} */
+void create_hdf5(){
+    hid_t       file_id, dataspace_id, dataset_id;   /* file identifier */
+    herr_t      status;
+    hsize_t dims[3];
 
-int main(int, char **)
+    int some_size = 128;
+
+    dims[0]      = some_size;
+    dims[1]      = some_size;
+    dims[2]      = some_size;
+
+    float (* dset_data)[some_size][some_size] = calloc(some_size, sizeof(dset_data));
+
+    if ( !dset_data )
+    {
+      fprintf( stderr, "Fatal: unable to allocate array\n" );
+      exit( EXIT_FAILURE );
+    }
+
+    /* Create a new file using default properties. */
+    printf("Create file \n");
+    file_id = H5Fcreate("data/datasets/test_dataset_hdf5-c.h5", H5F_ACC_TRUNC, H5P_DEFAULT, H5P_DEFAULT);
+
+    dataspace_id = H5Screate_simple (3, dims, NULL);
+
+    printf("Create dataset \n");
+    dataset_id = H5Dcreate2(file_id, "/X", H5T_IEEE_F64LE, dataspace_id, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT);
+
+    int i, j, k;
+    int a = 12;
+
+    printf("Fill with values \n");
+    for (i = 0; i < some_size; i++)
+        for (j = 0; j < some_size; j++)
+            for (k = 0; k < some_size; k++)
+                printf("i: %d, j: %d, k: %d \n", i, j, k);
+                dset_data[i][j][k] = (float)((double)rand()/(double)(RAND_MAX/a));
+
+    
+    printf("Write data to dataset \n");
+    //status = H5Dwrite(dataset_id, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, dset_data);
+
+    free(dset_data);
+
+    /* Terminate access to the file. */
+    status = H5Dclose (dataset_id);
+    status = H5Sclose (dataspace_id);
+    status = H5Fclose(file_id); 
+}
+
+int main(int argc, char **argv)
 {
 
-    bench_nczarr_open();
-    bench_netcdf4_open();
-    bench_hdf5_open();
+    // bench_nczarr_open();
+    // bench_netcdf4_open();
+    // bench_hdf5_open();
+
+    //create_hdf5_subfiling(argc, argv);
+    create_hdf5();
 
     return 0;
 }
