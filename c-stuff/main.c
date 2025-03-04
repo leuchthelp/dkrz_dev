b66371ee7d7594b86e5ce268bde0d95af37a0937
leucht <lucas.philipp@st.ovgu.de>
2025-03-04 18:31:23 +0100

    Fix issue with async

--- c-stuff/main.c
+++ c-stuff/main.c
@@ -228,23 +228,12 @@ void create_hdf5_subfiling(int argc, char **argv, int size)
     H5Fclose(file_id);
 }
 
-void create_hdf5_async(int argc, char **argv, bool with_chunking, int size)
+void create_hdf5_async(bool with_chunking, int size)
 {
     hid_t plist_id, file_id, dataspace_id, dataset_id, es_id; /* file identifier */
     herr_t status;
     hsize_t dims[1];
     hsize_t cdims[1];
-
-    int mpi_thread_required = MPI_THREAD_MULTIPLE;
-    int mpi_thread_provided = 0;
-    /* Initialize MPI with threading support */
-    MPI_Init_thread(&argc, &argv, mpi_thread_required, &mpi_thread_provided);
-
-    int rrank, ssize;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rrank);
-    MPI_Comm_size(MPI_COMM_WORLD, &ssize);
-
-    printf("MPI Rank = %d, MPI Size = %d\n", rrank, ssize);
 
     es_id = H5EScreate();
 
@@ -258,11 +247,6 @@ void create_hdf5_async(int argc, char **argv, bool with_chunking, int size)
     dataspace_id = H5Screate_simple(1, dims, NULL);
 
     plist_id = H5Pcreate(H5P_DATASET_CREATE);
-
-    MPI_Comm comm = MPI_COMM_WORLD;
-    MPI_Info info = MPI_INFO_NULL;
-
-    H5Pset_fapl_mpio(plist_id, comm, info);
 
     if (with_chunking)
     {
@@ -540,16 +524,17 @@ void bench_variable_subfiling(int argc, char **argv, int size, int iteration)
     // save_list_to_json(arr3, "test_hdf5_subfiling_list.json", "hdf5-subfiling-read", iteration);
 }
 
-void bench_variable_async(int argc, char **argv, int size, int iteration)
+void bench_variable_async(int size, int iteration)
 {
     double arr3[iteration];
 
     printf(ANSI_COLOR_YELLOW "Create hdf5 file with vol-async" ANSI_COLOR_RESET "\n");
-    create_hdf5_async(argc, argv, false, size);
+    create_hdf5_async(false, size);
     printf(ANSI_COLOR_YELLOW "Finish creating hdf5 file" ANSI_COLOR_RESET "\n");
 
     for (int i = 0; i < iteration; i++)
     {
+        printf("i: %d for variable: X\n", i);
         struct timespec start, end;
 
         clock_gettime(CLOCK_MONOTONIC, &start);
@@ -561,11 +546,6 @@ void bench_variable_async(int argc, char **argv, int size, int iteration)
         herr_t status;
         int some_size = size;
         float *rbuf = calloc(some_size, sizeof(float));
-
-        int rank;
-        MPI_Comm comm = MPI_COMM_WORLD;
-        MPI_Comm_rank(comm, &rank);
-        printf("iteration %d, rank: %d \n", i, rank);
 
         file_id = H5Fopen_async("data/datasets/test_dataset_hdf5-c.h5", H5F_ACC_RDONLY, H5P_DEFAULT, es_id);
         dataset_id = H5Dopen_async(file_id, "/X", H5P_DEFAULT, es_id);
@@ -831,11 +811,6 @@ static struct argp_option options[] = {
 
 int main(int argc, char **argv)
 {
-
-    create_hdf5_async(argc, argv, false, 134217728);
-    MPI_Finalize();
-
-    /*
     struct argp argp = {options, parse_opt};
 
     args_t arguments;
@@ -855,9 +830,7 @@ int main(int argc, char **argv)
 
     int size = arguments.size * arguments.factor;
     int iterations = arguments.iterations;
-    */
 
-    /*
     switch (arguments.benchmark)
     {
     case 1:
@@ -879,20 +852,15 @@ int main(int argc, char **argv)
         MPI_Finalize();
         break;
     case 5:
-        printf(ANSI_COLOR_RED "PLEASE ENSURE THIS SCRIPT IS RUN WITH MPIEXEC / MPIRUN" ANSI_COLOR_RESET "\n");
         printf(ANSI_COLOR_RED "ALSO ENSURE YOU LOEADED HDF5-VOL-ASYNC AND SET THE ENVIRONMENTAL VARIABLES REQUIRED CORRECTLY" ANSI_COLOR_RESET "\n");
-        printf(ANSI_COLOR_RED "FAILING TO DO SO WILL CAUSE HDF5 ERROR WHEN RUNNING WITH N>1 - PLEASE REFER TO THE README" ANSI_COLOR_RESET "\n");
-
         printf(ANSI_COLOR_BLUE "Running hdf5-vol-async benchmark with a filesize of %d for %d iterations" ANSI_COLOR_RESET "\n", size, iterations);
-        bench_variable_async(argc, argv, size, iterations);
-        MPI_Finalize();
+        bench_variable_async(size, iterations);
         break;
     case ARGP_KEY_ARG:
         return 0;
     default:
         return ARGP_ERR_UNKNOWN;
     }
-    */
 
     return 0;
 }
