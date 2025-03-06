68afa07b7a27165d52c55358b95b12d4202ea133
leucht <lucas.philipp@st.ovgu.de>
2025-03-06 13:19:32 +0100

    Trying to fix async

--- c-stuff/main.c
+++ c-stuff/main.c
@@ -68,6 +68,50 @@ void save_list_to_json(double *arr, char *file_name, char *name, size_t size)
     fclose(fptr);
 }
 
+int check_vol_async_present()
+{
+    hid_t default_vol_id = H5I_INVALID_HID;
+    hid_t native_vol_id = H5I_INVALID_HID;
+    int cmp = -1;
+
+    /* Get the ID of the VOL connector set on the default FAPL (H5P_DEFAULT) */
+    if (H5Pget_vol_id(H5P_DEFAULT, &default_vol_id) < 0)
+    {
+        fprintf(stderr, "error\n");
+        return -1;
+    }
+
+    /* Get the ID for the native VOL connector by its name */
+    if ((native_vol_id = H5VLget_connector_id_by_name("async")) < 0)
+    {
+        fprintf(stderr, "error\n");
+        return -1;
+    }
+
+    /* Compare the two VOL connectors */
+    if (H5VLcmp_connector_cls(&cmp, default_vol_id, native_vol_id) < 0)
+    {
+        fprintf(stderr, "error\n");
+        return -1;
+    }
+
+    if (0 == cmp)
+    {
+        printf("Using the async VOL connector\n");
+        return 0;
+    }
+    else
+    {
+        printf("NOT using the async VOL connector\n");
+        return -1;
+    }
+
+    H5VLclose(default_vol_id);
+    H5VLclose(native_vol_id);
+
+    return 0;
+}
+
 void create_hdf5(bool with_chunking, int size)
 {
     hid_t plist_id, file_id, filespace, dset_id; /* file identifier */
@@ -120,89 +164,6 @@ void create_hdf5(bool with_chunking, int size)
     status = H5Dclose(dset_id);
     status = H5Sclose(filespace);
     status = H5Fclose(file_id);
-}
-
-void create_hdf5_async(bool with_chunking, int size)
-{
-    hid_t plist_id = 0;
-    hid_t file_id = 0;
-    hid_t filespace = 0;
-    hid_t dset_id = 0;
-    hid_t es_id = 0;
-    herr_t status = -1;
-    hsize_t dims[1];
-    hsize_t cdims[1];
-
-    es_id = H5EScreate();
-
-    /*
-     * Set up file access property list with parallel I/O access
-     */
-    plist_id = H5Pcreate(H5P_FILE_ACCESS);
-
-    /*
-     * Create a new file collectively.
-     */
-    file_id = H5Fcreate_async("data/datasets/test_dataset_hdf5-c.h5", H5F_ACC_TRUNC, H5P_DEFAULT, plist_id, es_id);
-
-    /*
-     * Close property list.
-     */
-    H5Pclose(plist_id);
-
-    // setup dimensions
-    int some_size = size;
-
-    dims[0] = some_size;
-    filespace = H5Screate_simple(1, dims, NULL);
-
-    if (with_chunking)
-    {
-        // setup chunking
-        cdims[0] = 512;
-        status = H5Pset_chunk(plist_id, 1, cdims);
-    }
-
-    /*
-     * Create the dataset with default properties and close filespace.
-     */
-    dset_id = H5Dcreate_async(file_id, "/X", H5T_IEEE_F64LE, filespace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT, es_id);
-    H5Sclose(filespace);
-
-    /*
-     * Initialize data buffer
-     */
-    float *wbuf = calloc(some_size, sizeof(float));
-
-    if (!wbuf)
-    {
-        fprintf(stderr, "Fatal: unable to allocate array\n");
-        exit(EXIT_FAILURE);
-    }
-
-    int i;
-
-    for (i = 0; i < some_size; i++)
-    {
-        wbuf[i] = (float)rand() / RAND_MAX;
-    }
-
-    status = H5Dwrite_async(dset_id, H5T_NATIVE_FLOAT, H5S_ALL, H5S_ALL, H5P_DEFAULT, wbuf, es_id);
-
-    /*
-     * Close/release resources.
-     */
-    size_t num_in_progress;
-    hbool_t op_failed;
-    status = H5ESwait(es_id, H5ES_WAIT_FOREVER, &num_in_progress, &op_failed);
-    printf("Finish waiting for async, num in progess: %ld, failed: %d, status: %d \n", num_in_progress, op_failed, status);
-
-    status = H5Dclose_async(dset_id, es_id);
-    status = H5Fclose_async(file_id, es_id);
-
-    status = H5ESclose(es_id);
-
-    free(wbuf);
 }
 
 void create_hdf5_parallel(int argc, char **argv, bool with_chunking, int size)
@@ -325,9 +286,10 @@ void create_hdf5_parallel(int argc, char **argv, bool with_chunking, int size)
     MPI_Barrier(MPI_COMM_WORLD);
 }
 
-void create_hdf5_async_parallel(int argc, char **argv, bool with_chunking, int size)
+void create_hdf5_async(int argc, char **argv, bool with_chunking, int size)
 {
     hid_t plist_id = 0;
+    hid_t fapl_id = 0;
     hid_t file_id = 0;
     hid_t filespace = 0;
     hid_t memspace = 0;
@@ -361,41 +323,36 @@ void create_hdf5_async_parallel(int argc, char **argv, bool with_chunking, int s
     /*
      * Set up file access property list with parallel I/O access
      */
-    plist_id = H5Pcreate(H5P_FILE_ACCESS);
-    status = H5Pset_fapl_mpio(plist_id, comm, info);
+    fapl_id = H5Pcreate(H5P_FILE_ACCESS);
+    status = H5Pset_fapl_mpio(fapl_id, comm, info);
 
-    H5Pset_all_coll_metadata_ops(plist_id, true);
-    H5Pset_coll_metadata_write(plist_id, true);
+    H5Pset_all_coll_metadata_ops(fapl_id, true);
+    H5Pset_coll_metadata_write(fapl_id, true);
+
+    check_vol_async_present();
 
     /*
      * Create a new file collectively.
      */
-    file_id = H5Fcreate_async("data/datasets/test_dataset_hdf5-c_async.h5", H5F_ACC_TRUNC, H5P_DEFAULT, plist_id, es_id);
-
-    /*
-     * Close property list.
-     */
-    H5Pclose(plist_id);
+    file_id = H5Fcreate_async("data/datasets/test_dataset_hdf5-c_async.h5", H5F_ACC_TRUNC, H5P_DEFAULT, fapl_id, es_id);
 
     // setup dimensions
     int process_mem_size = size / mpi_size;
 
     dims[0] = size;
-    mem_chunk_dims[0] = process_mem_size;
     filespace = H5Screate_simple(1, dims, NULL);
 
     if (with_chunking)
     {
         // setup chunking
         cdims[0] = 512;
-        status = H5Pset_chunk(plist_id, 1, cdims);
+        status = H5Pset_chunk(fapl_id, 1, cdims);
     }
 
     /*
      * Create the dataset with default properties and close filespace.
      */
     dset_id = H5Dcreate_async(file_id, "/X", H5T_IEEE_F64LE, filespace, H5P_DEFAULT, H5P_DEFAULT, H5P_DEFAULT, es_id);
-    H5Sclose(filespace);
 
     /*
      * Select hyperslab in the file.
@@ -404,7 +361,6 @@ void create_hdf5_async_parallel(int argc, char **argv, bool with_chunking, int s
     offset[0] = count[0] * mpi_rank;
     memspace = H5Screate_simple(1, count, NULL);
 
-    filespace = H5Dget_space(dset_id);
     status = H5Sselect_hyperslab(filespace, H5S_SELECT_SET, offset, NULL, count, NULL);
     /*
      * Initialize data buffer
@@ -445,7 +401,9 @@ void create_hdf5_async_parallel(int argc, char **argv, bool with_chunking, int s
     status = H5ESwait(es_id, H5ES_WAIT_FOREVER, &num_in_progress, &op_failed);
     printf("Finish waiting for async, num in progess: %ld, failed: %d, status: %d \n", num_in_progress, op_failed, status);
 
+    status = H5Pclose(fapl_id);
     status = H5Dclose_async(dset_id, es_id);
+    status = H5Sclose(memspace);
     status = H5Sclose(filespace);
     status = H5Pclose(plist_id);
     status = H5Fclose_async(file_id, es_id);
@@ -826,94 +784,13 @@ void bench_variable_nczarr(int size, int iteration)
     save_to_json(arr3, "test_nczarr.json", "nczarr-read", iteration);
 }
 
-void bench_variable_async(int size, int iteration)
+void bench_variable_async(int argc, char **argv, int size, int iteration)
 {
     double arr3[iteration];
 
     printf(ANSI_COLOR_YELLOW "Create hdf5 file with vol-async" ANSI_COLOR_RESET "\n");
-    create_hdf5_async(false, size);
+    create_hdf5_async(argc, argv, false, size);
     printf(ANSI_COLOR_YELLOW "Finish creating hdf5 file" ANSI_COLOR_RESET "\n");
-
-    for (int i = 0; i < iteration; i++)
-    {
-        printf("i: %d for variable: X\n", i);
-        struct timespec start, end;
-
-        clock_gettime(CLOCK_MONOTONIC, &start);
-
-        hid_t es_id = 0;
-        hid_t file_id = 0;
-        hid_t dset_id = 0;
-        hid_t plist_id = 0;
-        herr_t status = -1;
-
-        es_id = H5EScreate();
-
-        /*
-         * Initialize data buffer
-         */
-        int some_size = size;
-        float *rbuf = calloc(some_size, sizeof(float));
-
-        /*
-         * Set up file access property list with parallel I/O access
-         */
-        plist_id = H5Pcreate(H5P_FILE_ACCESS);
-
-        /*
-         * Open existing file collectively and release property list identifier.
-         */
-        file_id = H5Fopen_async("data/datasets/test_dataset_hdf5-c.h5", H5F_ACC_RDONLY, plist_id, es_id);
-        H5Pclose(plist_id);
-
-        /*
-         * open the dataset with default properties and close filespace.
-         */
-        dset_id = H5Dopen_async(file_id, "/X", H5P_DEFAULT, es_id);
-
-        /*
-         * To read dataset independently use
-         *
-         * H5Pset_dxpl_mpio(plist_id, H5FD_MPIO_INDEPENDENT);
-         */
-        status = H5Dread_async(dset_id, H5T_NATIVE_FLOAT, H5S_BLOCK, H5S_ALL, H5P_DEFAULT, rbuf, es_id);
-
-        /*
-         * Close/release resources.
-         */
-        status = H5Dclose_async(dset_id, es_id);
-        status = H5Fclose_async(file_id, es_id);
-
-        size_t num_in_progress;
-        hbool_t op_failed;
-
-        printf("Wait for async answer \n");
-        status = H5ESwait(es_id, H5ES_WAIT_FOREVER, &num_in_progress, &op_failed);
-        printf("Finish waiting for async, num in progess: %ld, failed: %d, status: %d \n", num_in_progress, op_failed, status);
-
-        H5ESclose(es_id);
-
-        clock_gettime(CLOCK_MONOTONIC, &end);
-        double elapsed = end.tv_sec - start.tv_sec;
-        elapsed += (end.tv_nsec - start.tv_nsec) / 1000000000.0;
-        arr3[i] = elapsed;
-
-        free(rbuf);
-        printf(ANSI_COLOR_GREEN "Finished" ANSI_COLOR_RESET "\n");
-    }
-
-    save_to_json(arr3, "test_hdf5-c_async.json", "hdf5-c-async-read", iteration);
-}
-
-void bench_variable_async_parallel(int argc, char **argv, int size, int iteration)
-{
-    double arr3[iteration];
-
-    /*
-    printf(ANSI_COLOR_YELLOW "Create hdf5 file with vol-async" ANSI_COLOR_RESET "\n");
-    create_hdf5_async_parallel(argc, argv, false, size);
-    printf(ANSI_COLOR_YELLOW "Finish creating hdf5 file" ANSI_COLOR_RESET "\n");
-    */
 
     /*
      * Initialize MPI
@@ -923,10 +800,10 @@ void bench_variable_async_parallel(int argc, char **argv, int size, int iteratio
     MPI_Comm comm = MPI_COMM_WORLD;
     MPI_Info info = MPI_INFO_NULL;
 
-    int mpi_thread_required = MPI_THREAD_MULTIPLE;
-    int mpi_thread_provided = 0;
+    // int mpi_thread_required = MPI_THREAD_MULTIPLE;
+    // int mpi_thread_provided = 0;
     /* Initialize MPI with threading support */
-    MPI_Init_thread(&argc, &argv, mpi_thread_required, &mpi_thread_provided);
+    // MPI_Init_thread(&argc, &argv, mpi_thread_required, &mpi_thread_provided);
 
     MPI_Comm_size(comm, &mpi_size);
     MPI_Comm_rank(comm, &mpi_rank);
@@ -959,7 +836,7 @@ void bench_variable_async_parallel(int argc, char **argv, int size, int iteratio
         /*
          * Open existing file collectively and release property list identifier.
          */
-        file_id = H5Fopen_async("data/datasets/test_dataset_hdf5-c.h5", H5F_ACC_RDONLY, plist_id, es_id);
+        file_id = H5Fopen_async("data/datasets/test_dataset_hdf5-c_async.h5", H5F_ACC_RDONLY, plist_id, es_id);
         H5Pclose(plist_id);
 
         /*
@@ -1023,7 +900,7 @@ void bench_variable_async_parallel(int argc, char **argv, int size, int iteratio
         printf(ANSI_COLOR_GREEN "Finished" ANSI_COLOR_RESET "\n");
     }
 
-    save_to_json(arr3, "test_hdf5-c_async_parallel.json", "hdf5-c-async-read-parallel", iteration);
+    save_to_json(arr3, "test_hdf5-c_async.json", "hdf5-c-async-read", iteration);
 }
 
 void bench_variable_subfiling(int argc, char **argv, int size, int iteration)
@@ -1235,27 +1112,23 @@ int main(int argc, char **argv)
         bench_variable_nczarr(size, iterations);
         break;
     case 4:
-        printf(ANSI_COLOR_RED "PLEASE ENSURE YOU LOADED HDF5-VOL-ASYNC AND SET THE ENVIRONMENTAL VARIABLES REQUIRED CORRECTLY" ANSI_COLOR_RESET "\n");
-        printf(ANSI_COLOR_BLUE "Running hdf5-vol-async benchmark with a filesize of %d for %d iterations" ANSI_COLOR_RESET "\n", size, iterations);
-        bench_variable_async(size, iterations);
-        break;
-    case 5:
         printf(ANSI_COLOR_RED "CURRENTLY ONLY WORKS WITH EVEN MPI_RANKS" ANSI_COLOR_RESET "\n");
         printf(ANSI_COLOR_BLUE "Running hdf5 parallel benchmark with a filesize of %d for %d iterations" ANSI_COLOR_RESET "\n", size, iterations);
         bench_variable_hdf5_parallel(argc, argv, size, iterations);
         MPI_Finalize();
         break;
-    case 6:
+    case 5:
         printf(ANSI_COLOR_RED "CURRENTLY ONLY WORKS WITH EVEN MPI_RANKS" ANSI_COLOR_RESET "\n");
         printf(ANSI_COLOR_RED "PLEASE ENSURE YOU LOADED HDF5-VOL-ASYNC AND SET THE ENVIRONMENTAL VARIABLES REQUIRED CORRECTLY" ANSI_COLOR_RESET "\n");
         printf(ANSI_COLOR_BLUE "Running hdf5-vol-async parallel benchmark with a filesize of %d for %d iterations" ANSI_COLOR_RESET "\n", size, iterations);
-        bench_variable_async_parallel(argc, argv, size, iterations);
+        bench_variable_async(argc, argv, size, iterations);
         MPI_Finalize();
         break;
-    case 7:
+    case 6:
         printf(ANSI_COLOR_RED "CURRENTLY ONLY WORKS WITH EVEN MPI_RANKS" ANSI_COLOR_RESET "\n");
         printf(ANSI_COLOR_RED "PLEASE ENSURE THIS SCRIPT IS RUN WITH MPIEXEC / MPIRUN" ANSI_COLOR_RESET "\n");
         printf(ANSI_COLOR_BLUE "Running hdf5 subfiling benchmark with a filesize of %d for %d iterations" ANSI_COLOR_RESET "\n", size, iterations);
+
         bench_variable_subfiling(argc, argv, size, iterations);
         MPI_Finalize();
         break;
