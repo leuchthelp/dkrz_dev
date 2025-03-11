a3b4fe0954efb9103627c4d328aaa89ba9c6ed97
leucht <lucas.philipp@st.ovgu.de>
2025-03-11 17:55:05 +0100

    fix

--- dkrz_dev_setup.sh
+++ dkrz_dev_setup.sh
@@ -11,16 +11,16 @@ git clone --depth=2 --branch=releases/v0.23 https://github.com/spack/spack.git ~
 . spack/share/spack/setup-env.sh
 . $SPACK_ROOT/share/spack/setup-env.sh
 
+spack install gcc@11.4.0
+spack compiler add "$(spack location -i gcc@11.4.0)"
+
 # Activate creation of module files via spack
 spack config add "modules:default:enable:[tcl]"
 
 # Install and setup "module" and add to local shell
-spack install lmod
+spack install lmod %gcc@11.4.0
 . $(spack location -i lmod)/lmod/lmod/init/bash
 . $SPACK_ROOT/share/spack/setup-env.sh
-
-spack install gcc@11.4.0
-spack compiler add "$(spack location -i gcc@11.4.0)"
 
 spack install python@3.11.9 %gcc@11.4.0
 spack install openmpi %gcc@11.4.0
