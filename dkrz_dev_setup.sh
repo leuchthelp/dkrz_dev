764ea9295dc461b1bcea0a011a71afe5fa4485bd
leucht <lucas.philipp@st.ovgu.de>
2025-03-15 14:27:01 +0100

    fix

--- dkrz_dev_setup.sh
+++ dkrz_dev_setup.sh
@@ -11,15 +11,15 @@ git clone --depth=2 --branch=releases/v0.23 https://github.com/spack/spack.git ~
 . spack/share/spack/setup-env.sh
 . $SPACK_ROOT/share/spack/setup-env.sh
 
-#echo remove gcc@11.4.0 compiler if present and rebuild with spack
-#echo simple sanity check to ensure matching compiler is correctly installed and available
-#spack compiler remove gcc@11.4.0
+echo remove gcc@11.4.0 compiler if present and rebuild with spack
+echo simple sanity check to ensure matching compiler is correctly installed and available
+spack compiler remove gcc@11.4.0
 
 # Activate creation of module files via spack
 spack config add "modules:default:enable:[tcl]"
 
-#spack install --fresh gcc@11.4.0
-#spack compiler add "$(spack location -i gcc@11.4.0)"
+spack install --fresh gcc@11.4.0
+spack compiler add "$(spack location -i gcc@11.4.0)"
 
 spack compilers
 
