6c936afac23f9cdc1e4702d209314d2010efd15d
leucht <lucas.philipp@st.ovgu.de>
2025-03-15 13:29:15 +0100

    Add err to slurm

--- dkrz_dev_slurm.sh
+++ dkrz_dev_slurm.sh
@@ -2,11 +2,12 @@
 #SBATCH --partition=compute
 #SBATCH --account=ku0598
 #SBATCH --nodes=1
-#SBATCH --ntasks-per-node=6
+#SBATCH --ntasks-per-node=1
 #SBATCH --time=01:00:00
 #SBATCH --output=log.%j.txt
+#SBATCH --error=log.%j.err
 
 # Begin of section with executable commands
 set -e
 ls -l
-srun ./main.py
+srun python main.py
