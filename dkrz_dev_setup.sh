4a178d1145e36a432eb6705bac0e18756ff2b0ba
leucht <lucas.philipp@st.ovgu.de>
2025-03-22 13:33:54 +0100

    make setup a bit more friendly to configure

--- dkrz_dev_setup.sh
+++ dkrz_dev_setup.sh
@@ -6,6 +6,8 @@
 #sudo apt update
 #sudo apt install bzip2 ca-certificates g++ gcc gfortran git gzip lsb-release patch python3 tar unzip xz-utils zstd
 
+compiler=apple-clang
+
 # Install spack and add to local shell
 git clone --depth=2 --branch=releases/v0.23 https://github.com/spack/spack.git ~/spack
 . spack/share/spack/setup-env.sh
@@ -21,43 +23,43 @@ spack config add "modules:default:enable:[tcl]"
 #spack install --fresh gcc@11.4.0
 
 # change this for you compiler of choice, has to be done for compatibility with levant.dkrz.de
-spack compiler add "$(spack location -i gcc@11.2.0%gcc@11.2.0)"
+spack compiler add "$(spack location -i $compiler%$compiler)"
 
 spack compilers
 
 # Install and setup "module" and add to local shell
-spack install --fresh lmod %gcc@11.2.0
-. $(spack location -i lmod)/lmod/lmod/init/bash
+spack install --fresh lmod %$compiler
+. $(spack location -i lmod)/lmod/lmod/init/profile
 . $SPACK_ROOT/share/spack/setup-env.sh
 
 # additionals for my own comfort
-spack install --fresh git %gcc@11.2.0
+spack install --fresh git %$compiler
 module load git
-spack install --fresh nano %gcc@11.2.0 
+spack install --fresh nano %$compiler 
 
-spack install --fresh python@3.11.9 %gcc@11.2.0
+spack install --fresh python@3.11.9 %$compiler
 module load python
-spack install --fresh openmpi %gcc@11.2.0
+spack install --fresh openmpi %$compiler
 module load openmpi
-spack install --fresh hdf5@1.14.5~cxx~fortran+hl~ipo~java~map+mpi+shared+subfiling~szip+threadsafe+tools %gcc@11.2.0
+spack install --fresh hdf5@1.14.5~cxx~fortran+hl~ipo~java~map+mpi+shared+subfiling~szip+threadsafe+tools %$compiler
 module load hdf5
-spack install --fresh argobots@main %gcc@11.2.0
+spack install --fresh argobots@main %$compiler
 module load argobots
-spack install hdf5-vol-async@develop %gcc@11.2.0 
+spack install hdf5-vol-async@develop %$compiler 
 module load hdf5-vol-async
-spack install netcdf-c build_system=cmake %gcc@11.2.0
+spack install netcdf-c build_system=cmake %$compiler
 module load netcdf-c
-spack install py-pip %gcc@11.2.0
+spack install py-pip %$compiler
 module load py-pip
-spack install py-netcdf4 %gcc@11.2.0
+spack install py-netcdf4 %$compiler
 module load py-netcdf4
-spack install py-h5py %gcc@11.2.0
+spack install py-h5py %$compiler
 module load py-h5py
-spack install py-mpi4py %gcc@11.2.0
+spack install py-mpi4py %$compiler
 module load py-mpi4py
-spack install py-numpy %gcc@11.2.0
+spack install py-numpy %$compiler
 module load py-numpy
-spack install py-rich %gcc@11.2.0
+spack install py-rich %$compiler
 module load py-rich
 
 # Add to end of home/user/.bashrc via nano .bashrc from within your home/user directory
