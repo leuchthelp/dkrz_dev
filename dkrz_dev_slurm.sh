21ee966d381470cd60206da670568aa28b997f86
leucht <lucas.philipp@st.ovgu.de>
2025-05-01 13:52:48 +0000

    Small fix to general purpose slurm script

--- dkrz_dev_slurm.sh
+++ dkrz_dev_slurm.sh
@@ -4,14 +4,12 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBACTH --mem=0
-#SBATCH --cpu-freq=High
-#SBATCH --distribution=block:cyclic
 #SBATCH --time=08:00:00
-#SBATCH --output=log/log-%j/log.%j.txt
-#SBATCH --error=log/log-%j/log.%j.err
+#SBATCH --output=log-/log-%j/log.%j.txt
+#SBATCH --error=log-/log-%j/log.%j.err
 
 
 # Begin of section with executable commands
 set -e
 ls -l
-srun --cpu_bind=cores --cpu-freq=High --distribution=block:cyclic python main_cluster.py
+python main_cluster.py
