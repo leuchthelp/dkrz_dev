53a8c2f6192ce3c5907a797e0ebce5a5021888a7
leucht <lucas.philipp@st.ovgu.de>
2025-08-22 15:38:52 +0200

    Support parallel, parallel backend testing. Will matter in the future and done dynamically

--- components/handler/handler.py
+++ components/handler/handler.py
@@ -26,7 +26,17 @@ class Handler:
         
         self._load_config(path_to_config)
         self._check_paths()
-        self._check_supported_languages()
+        
+        parallel = False
+        try:
+            parallel = self.config["parallel"] 
+            if parallel != "Both" and type(parallel) is not bool: raise ValueError(bcolors.FAIL + "\"parallel\" can only either be \"True\", \"False\" or \"Both\"" + bcolors.ENDC)
+            
+        except KeyError:
+            print(bcolors.WARNING + f"\"parallel\" is unset! Be aware parallel will be automatically set to False as long as it remains unset. You will be unable to run parallelized benchmarks until you set it to True." + bcolors.ENDC)   
+        
+        
+        self._create_benchmark(parallel=parallel)
              
              
     def print_config(self):
@@ -54,7 +64,6 @@ class Handler:
     
     def _check_paths(self):
         print(bcolors.OKBLUE + "Check configured paths" + bcolors.ENDC)
-        failed = False
         for key, path in self.config["paths"].items():
             if not os.path.exists(path): raise ValueError(bcolors.FAIL + f"Configured path: {path} for key: {key} does not exist. Please create it." + bcolors.ENDC)
         
@@ -63,46 +72,80 @@ class Handler:
         print(bcolors.OKBLUE + "Create benchmarks" + bcolors.ENDC)
     
     
-    def _check_supported_languages(self):      
-        supported = ["c", "python"]
+    def _create_benchmark(self, parallel: str | bool):
+        
+        if parallel == "Both":
+            print(f"parallel {parallel} only matters in the future, for now ignore")
+            parallel = False
+        self._check_supported_languages(parallel=parallel)
+    
+    
+    def _check_supported_par_backend(self):
+        try:
+            backend = {
+                "MPI": self._check_mpi,
+                "Dask": self._check_dask,
+            }
+                               
+            par_backend = self.config["par_backend"]                    
+            if par_backend not in backend and par_backend != "All": raise ValueError(bcolors.FAIL + f"Parallel backend \"{par_backend}\" selected is not supported. Did you mean \"All\" to run all available backends?" + bcolors.ENDC)  
+            
+            for imported_backend, import_func in backend.items():
+                    
+                if par_backend == imported_backend or par_backend == "All":
+                    import_func()
+                             
+        except KeyError:
+            raise ValueError(bcolors.FAIL + f"\"parallel\" was set to True but not parallel backend has been configured within the config.yaml. Please select one of the available backends." + bcolors.ENDC)
+    
+     
+    def _check_supported_languages(self, parallel: bool):  
+        supported = None
+            
+        if parallel is False: 
+            supported = ["c", "python"] 
+        else:
+            supported = ["c", "python"]
         
         for language in self.config["languages"]:     
             if language not in supported: raise ValueError(bcolors.FAIL + f"Language: {language} not currently supported. If you want to help extend support please visit ..." + bcolors.ENDC)
             
             print(bcolors.OKBLUE + f"Language: {language} is supported" + bcolors.ENDC)
-            self._check_supported_formats(language=language)
+            self._check_supported_formats(language=language, parallel=parallel)
+       
     
-    def _check_supported_formats(self, language: str):     
-        supported = ["hdf5", "zarr", "netcdf4"]
+    def _check_supported_formats(self, language: str, parallel: bool):   
+        supported = None
+        
+        if  parallel is False:
+            supported = ["hdf5", "zarr", "netcdf4"]
+        else:
+            supported = ["hdf5", "zarr", "netcdf4"]
         
         for format in self.config["formats"]:         
             if format not in supported: raise ValueError(bcolors.FAIL + f"Format: {format} not currently supported. If you want to help extend support please visit ..." + bcolors.ENDC)
                 
             print(bcolors.OKBLUE + f"Format: {format} is supported." + bcolors.ENDC)
-            self._create_benchmark_manager(language=language, format=format)
-
+            self._create_benchmark_manager(language=language, format=format, parallel=parallel)
         
-    def _create_benchmark_manager(self, language: str, format: str):
+                                             
+    def _create_benchmark_manager(self, language: str, format: str, parallel = False, par_backend = None):
         for _, run in self.config["runs"].items():
-            
-            parallel = False
-            par_backend = None
-            try:
-                parallel = self.config["parallel"]
-                
-                if parallel is True: 
-                    try:
-                        par_backend = self.config["par_backend"]
-                    except KeyError:
-                        raise ValueError(bcolors.FAIL + f"\"parallel \" was set to {parallel} but not parallel backend has been configured within the config.yaml. Please select one of the available backends." + bcolors.ENDC)
-            
-            except KeyError:
-                print(bcolors.WARNING + f"\"parallel \" is unset! Be aware parallel will be automatically set to False as long as it remains unset. You will be unable to run parallelized benchmarks until you set it to True." + bcolors.ENDC)
-                parallel = False
             
             range       = self.config["range"]
             stepsize    = self.config["stepsize"]
             iterations  = self.config["iterations"]
-            
+                    
             print(bcolors.OKBLUE + f"Managing Benchmark with; file-structure: {run} parallel: {parallel}, par_backend: {par_backend}, language: {language}, format: {format}, range: {range}, stepsize: {stepsize} and {iterations} iterations" + bcolors.ENDC)
-            BenchmarkManager(handler_id=str(self.__uuid), run=run, parallel=parallel, par_backend=par_backend, language=language, format=format, range=range, stepsize=stepsize, iterations=iterations)
+            BenchmarkManager(handler_id=str(self.__uuid), run=run, parallel=parallel, par_backend=par_backend, language=language, format=format, range=range, stepsize=stepsize, iterations=iterations)              
+            
+    
+    def _check_mpi(self):
+        from mpi4py import MPI
+    
+    
+    def _check_dask(self):
+        try:
+            print("Import Dask (mock)")
+        except ImportError:
+            raise ImportError(bcolors.FAIL + f"Dask not present, please install Dask." + bcolors.ENDC)
--- components/python/benchmarks/benchmark_manager.py
+++ components/python/benchmarks/benchmark_manager.py
@@ -40,18 +40,9 @@ class BenchmarkManager:
         self._node       = int
         self._node_info  = yaml
         self._profiler   = bool
-        
-
-        if parallel:
-            if self._check_par_backend(): raise ValueError(bcolors.FAIL + f"Parallel backend {self.par_backend} selected is not supported" + bcolors.ENDC)
 
         self._run_benchmark()
     
-    
-    def _check_par_backend(self) -> bool:
-        
-        print(f"Something went wrong while trying to check of parallel backende {self.par_backend}")
-        return True
     
     def _run_benchmark(self):
         
