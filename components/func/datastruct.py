6561a4cb6672015a701bd74e4bc2ba7077360163
leucht <lucas.philipp@st.ovgu.de>
2025-09-20 19:54:16 +0200

    Make helper Datastruct more robust

--- components/func/datastruct.py
+++ components/func/datastruct.py
@@ -20,7 +20,7 @@ class bcolors:
 class Datastruct:
     
     
-    def __init__(self, path=None, shape=[], chunks=[], mode=None, engine=None, compression=None, dataset=any, log=None, parallel=False):
+    def __init__(self, path=None, shape=[], chunks=[], mode=None, engine=None, compression=None, dataset=any, log=False, parallel=False):
         self.path = path
         self.shape = shape
         self.chunks = chunks
@@ -32,135 +32,232 @@ class Datastruct:
         self.parallel = parallel
         
     
-    def create(self, path, form, engine, dtype="f8", parallel=False):
+    def create(self, path: str, form: dict, engine: str, parallel=False, dtype="f8"):
 
         if type(engine) == str:
             self.engine = engine
             
         self.parallel = parallel
-        
-        if self.parallel == True:
-            from mpi4py import MPI
                 
             
         match self.engine:
             case "zarr":
-                if type(path) == str:
-                    self.path = path
-                    
-                from mpi4py import MPI
-                if MPI.COMM_WORLD.rank == 0 or parallel == False: # type: ignore
-                    root = zarr.create_group(store=path, zarr_format=3, overwrite=True)
-
-                    for variable, element in form.items():
-                        shape = element[0]
-                        chunks = element[1]
-                        
-                        if len(chunks) != 0:
-                            x = root.create_array(name=variable, shape=shape, chunks=chunks, dtype=dtype)
-                        else: 
-                            x = root.create_array(name=variable, shape=shape, dtype=dtype)
-                        
-                        x[:] = np.random.random_sample(shape)
-
-                    self.dataset = root
-                    print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
+                if self.parallel == False:
+                    self.create_zarr(form=form, path=path, dtype=dtype)
+                else:
+                    self.create_zarr_parallel(form=form, path=path, dtype=dtype)
             
         
             case "hdf5":
-                if type(path) == str:
-                    self.path = path
-                
-                # Create file either through mpio or serial
-                if self.parallel == True:
-                    print(f"{bcolors.WARNING}Creating hdf5 file{bcolors.ENDC}")
-                    root = h5py.File(path, "w-", driver="mpio", comm=MPI.COMM_WORLD) # type: ignore
+                if self.parallel == False:
+                    self.create_hdf5(form=form, path=path, dtype=dtype)
                 else:
-                    root = h5py.File(path, "w-")
-                    
-                    
-                # Create dataset corresponding to the provide number of variables
-                for variable, element in form.items():      
-                    shape = element[0]
-                    chunks = element[1]
-                    
-                    if len(chunks) != 0: 
-                        x = root.create_dataset(variable, shape=shape, chunks=tuple(chunks), dtype=dtype)
-                    else:
-                        x = root.create_dataset(variable, shape=shape, dtype=dtype)
-                    
-                    
-                    # File created dataset with values
-                    if self.parallel == False:
-                        x[:] = np.random.random_sample(shape)   
-                    else:
-                        rank = MPI.COMM_WORLD.rank # type: ignore
-                        rsize = MPI.COMM_WORLD.size # type: ignore
-                        total_size = shape[0]
-                        size = int(total_size / rsize)
-                        
-                        rstart = rank * size
-                        rend = rstart + size
-                        
-                        x[rstart:rend:] = np.random.random_sample(size)
-                        MPI.COMM_WORLD.Barrier() # type: ignore
-                
-        
-                self.dataset = root
-                root.close()
-                print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
+                    self.create_hdf5_parallel(form=form, path=path, dtype=dtype)
             
         
             case "netcdf4":
-                if type(path) == str:
-                    self.path = path
-                    
-                
-                root = netCDF4.Dataset(path, "w", format="NETCDF4", parallel=parallel)  # type: ignore
-
-                root.createGroup("/")
-                used = 0
-                
-                for variable, element in form.items():
-                    shape = element[0]
-                    chunks = element[1]
-                    dimensions = []
-                    
-                    for size in shape:
-                        root.createDimension(f"{used}", size)
-                        dimensions.append(f"{used}")
-                        used += 1
-                    
-                    if len(chunks) != 0: 
-                        x = root.createVariable(variable, dtype, dimensions, chunksizes=chunks)
-                    else: 
-                        x = root.createVariable(variable, dtype, dimensions)
-                    
-                    if self.parallel == False:
-                        x[:] = np.random.random_sample(shape)
-                    else:
-                        rank = MPI.COMM_WORLD.rank  # type: ignore
-                        rsize = MPI.COMM_WORLD.size  # type: ignore
-                        total_size = shape[0]
-                        size = int(total_size / rsize)
-                        
-                        rstart = rank * size
-                        rend = rstart + size
-                        
-                        #x.set_collective(True)
-                        x[rstart:rend:] = np.random.random_sample(size)
-                        MPI.COMM_WORLD.Barrier()  # type: ignore
-                          
-                          
-                self.dataset = root
-                root.close()
-                print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
+                if self.parallel == False:
+                    self.create_netcdf4(form=form, path=path, dtype=dtype)
+                else:
+                    self.create_netcdf4_parallel(form=form, path=path, dtype=dtype)
                 
                 
         return self
+ 
+    
+    def create_zarr(self, form: dict, path: str, dtype: str):
+        root = zarr.create_group(store=path, zarr_format=3, overwrite=True)
+        
+        for variable, element in form.items():
+            shape = element[0]
+            chunks = element[1]
+            
+            if len(chunks) != 0:
+                x = root.create_array(name=variable, shape=shape, chunks=chunks, dtype=dtype)
+            else: 
+                x = root.create_array(name=variable, shape=shape, dtype=dtype)
+            
+            x[:] = np.random.random_sample(shape)
+        self.dataset = root
+        print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
+        
+        return self
+    
+    
+    def create_zarr_parallel(self, form: dict, path: str, dtype: str):
+        from mpi4py import MPI
+        if MPI.COMM_WORLD.rank == 0: # type: ignore
+            
+            root = zarr.create_group(store=path, zarr_format=3, overwrite=True)
+            
+            for variable, element in form.items():
+                shape = element[0]
+                chunks = element[1]
+                
+                if len(chunks) != 0:
+                    x = root.create_array(name=variable, shape=shape, chunks=chunks, dtype=dtype)
+                else: 
+                    x = root.create_array(name=variable, shape=shape, dtype=dtype)
+                
+                x[:] = np.random.random_sample(shape)
+            self.dataset = root
+            print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
+    
+        return self
+    
+    
+    def create_hdf5(self, form: dict, path: str, dtype: str):
+        if type(path) == str:
+            self.path = path
+        
+        # Create file either through mpio or serial
+        print(f"{bcolors.WARNING}Creating hdf5 file{bcolors.ENDC}")
+        root = h5py.File(path, "w-")
+            
+            
+        # Create dataset corresponding to the provide number of variables
+        for variable, element in form.items():      
+            shape = element[0]
+            chunks = element[1]
+            
+            if len(chunks) != 0: 
+                x = root.create_dataset(variable, shape=shape, chunks=tuple(chunks), dtype=dtype)
+            else:
+                x = root.create_dataset(variable, shape=shape, dtype=dtype)
+            
+            
+            # File created dataset with values
+            x[:] = np.random.random_sample(shape)   
+
+        self.dataset = root
+        root.close()
+        print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
+        
+        return self
+    
+    
+    def create_hdf5_parallel(self, form: dict, path: str, dtype: str):
+        from mpi4py import MPI
+        
+        if type(path) == str:
+            self.path = path
+        
+        # Create file either through mpio or serial
+        print(f"{bcolors.WARNING}Creating hdf5 file{bcolors.ENDC}")
+        root = h5py.File(path, "w-", driver="mpio", comm=MPI.COMM_WORLD) # type: ignore
+            
+            
+        # Create dataset corresponding to the provide number of variables
+        for variable, element in form.items():      
+            shape = element[0]
+            chunks = element[1]
+            
+            if len(chunks) != 0: 
+                x = root.create_dataset(variable, shape=shape, chunks=tuple(chunks), dtype=dtype)
+            else:
+                x = root.create_dataset(variable, shape=shape, dtype=dtype)
+            
+            
+            # File created dataset with values
+            rank = MPI.COMM_WORLD.rank # type: ignore
+            rsize = MPI.COMM_WORLD.size # type: ignore
+            total_size = shape[0]
+            size = int(total_size / rsize)
+            
+            rstart = rank * size
+            rend = rstart + size
+            
+            x[rstart:rend:] = np.random.random_sample(size)
+            MPI.COMM_WORLD.Barrier() # type: ignore
+
+        self.dataset = root
+        root.close()
+        print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
+        
+        return self
+    
+    
+    def create_netcdf4(self, form: dict, path: str, dtype: str):
+        if type(path) == str:
+            self.path = path
+                    
+                
+        root = netCDF4.Dataset(path, "w", format="NETCDF4")  # type: ignore
+        root.createGroup("/")
+        used = 0
+        
+        for variable, element in form.items():
+            shape = element[0]
+            chunks = element[1]
+            dimensions = []
+            
+            for size in shape:
+                root.createDimension(f"{used}", size)
+                dimensions.append(f"{used}")
+                used += 1
+            
+            if len(chunks) != 0: 
+                x = root.createVariable(variable, dtype, dimensions, chunksizes=chunks)
+            else: 
+                x = root.createVariable(variable, dtype, dimensions)
+            
+            
+            x[:] = np.random.random_sample(shape)
+         
+                  
+        self.dataset = root
+        root.close()
+        print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
+        
+        return self
+    
+    
+    def create_netcdf4_parallel(self, form: dict, path: str, dtype: str):
+        if type(path) == str:
+            self.path = path
+                    
+                
+        root = netCDF4.Dataset(path, "w", format="NETCDF4", parallel=True)  # type: ignore
+        root.createGroup("/")
+        used = 0
+        
+        for variable, element in form.items():
+            shape = element[0]
+            chunks = element[1]
+            dimensions = []
+            
+            for size in shape:
+                root.createDimension(f"{used}", size)
+                dimensions.append(f"{used}")
+                used += 1
+            
+            if len(chunks) != 0: 
+                x = root.createVariable(variable, dtype, dimensions, chunksizes=chunks)
+            else: 
+                x = root.createVariable(variable, dtype, dimensions)
+            
+
+            rank = MPI.COMM_WORLD.rank  # type: ignore
+            rsize = MPI.COMM_WORLD.size  # type: ignore
+            total_size = shape[0]
+            size = int(total_size / rsize)
+            
+            rstart = rank * size
+            rend = rstart + size
+            
+            x.set_collective(True)
+            x[rstart:rend:] = np.random.random_sample(size)
+            MPI.COMM_WORLD.Barrier()  # type: ignore
+                  
+                  
+        self.dataset = root
+        root.close()
+        print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
+        
+        return self
         
         
-    def open(self, mode, engine = None | str, path = None | str, parallel=False):
+    def open(self, mode: str, engine: None | str, path: None | str, parallel=False):
         
         self.parallel = parallel
         
@@ -195,7 +292,7 @@ class Datastruct:
         return self
 
                 
-    def __bench_variable(self, variable: list, iterations):
+    def __bench_variable(self, variable: list, iterations: int):
         bench = []
         
         match self.engine:
@@ -222,7 +319,7 @@ class Datastruct:
                     self.dataset[variable][:]  # type: ignore
                     bench.append(time.monotonic() - start)
                 
-                self.dataset.close()
+                self.dataset.close()  # type: ignore
                 self.log = bench
                 print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
                 
@@ -236,12 +333,12 @@ class Datastruct:
                     self.dataset[variable][:]  # type: ignore
                     bench.append(time.monotonic() - start)
                 
-                self.dataset.close()
+                self.dataset.close()  # type: ignore
                 self.log = bench
                 print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
  
  
-    def __bench_variable_parallel(self, variable: list, iterations):
+    def __bench_variable_parallel(self, variable: list, iterations: int):
         from mpi4py import MPI
         
         bench = []
@@ -300,7 +397,7 @@ class Datastruct:
                 if rank == 0:
                     self.log = bench
                 
-                self.dataset.close()   
+                self.dataset.close()  # type: ignore
                 MPI.COMM_WORLD.Barrier()
                 print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
                 
@@ -331,12 +428,12 @@ class Datastruct:
                 if rank == 0:
                     self.log = bench
                 
-                self.dataset.close()    
+                self.dataset.close()  # type: ignore  
                 MPI.COMM_WORLD.Barrier()
                 print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
   
   
-    def __bench_complete(self, variable: list, iterations):
+    def __bench_complete(self, variable: list, iterations: int):
         bench = []
         size = []
         var_tmp = []
@@ -387,7 +484,7 @@ class Datastruct:
                         
                     bench.append(time.monotonic() - start)
                 
-                self.dataset.close()
+                self.dataset.close()  # type: ignore
                 self.log = bench
                 print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
                 
@@ -411,12 +508,12 @@ class Datastruct:
                     
                     bench.append(time.monotonic() - start)
                 
-                self.dataset.close()
+                self.dataset.close()  # type: ignore
                 self.log = bench
                 print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
  
  
-    def __bench_complete_parallel(self, variable: list, iterations):  
+    def __bench_complete_parallel(self, variable: list, iterations: int):  
         match self.engine:
             case "zarr":
                 self.__bench_complete_parallel_zarr(variable=variable, iterations=iterations)
@@ -428,10 +525,11 @@ class Datastruct:
                 self.__bench_complete_parallel_netcdf4(variable=variable, iterations=iterations)
  
     
-    def __bench_complete_parallel_zarr(self, variable: list, iterations):
+    def __bench_complete_parallel_zarr(self, variable: list, iterations: int):
         bench = []
         size = []
         var_tmp = []
+        
         from mpi4py import MPI  
         rank = MPI.COMM_WORLD.rank
         rsize = MPI.COMM_WORLD.size
@@ -474,10 +572,11 @@ class Datastruct:
         print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
      
         
-    def __bench_complete_parallel_hdf5(self, variable: list, iterations): 
+    def __bench_complete_parallel_hdf5(self, variable: list, iterations: int): 
         bench = []
         size = []
         var_tmp = []
+        
         from mpi4py import MPI  
         rank = MPI.COMM_WORLD.rank
         rsize = MPI.COMM_WORLD.size
@@ -515,15 +614,16 @@ class Datastruct:
         if rank == 0:
             self.log = bench
          
-        self.dataset.close()   
+        self.dataset.close()  # type: ignore 
         MPI.COMM_WORLD.Barrier()
         print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
         
         
-    def __bench_complete_parallel_netcdf4(self, variable: list, iterations): 
+    def __bench_complete_parallel_netcdf4(self, variable: list, iterations: int): 
         bench = []
         size = []
         var_tmp = []
+        
         from mpi4py import MPI  
         rank = MPI.COMM_WORLD.rank
         rsize = MPI.COMM_WORLD.size
@@ -544,7 +644,7 @@ class Datastruct:
             for var in variable:
                 
                 try:
-                    #self.dataset[var].set_collective(True)  # type: ignore
+                    self.dataset[var].set_collective(True)  # type: ignore
                     total_size = self.dataset[var].shape[0]  # type: ignore
                     size = int(total_size / rsize)
 
@@ -563,12 +663,12 @@ class Datastruct:
         if rank == 0:
             self.log = bench
         
-        self.dataset.close()
+        self.dataset.close()  # type: ignore
         MPI.COMM_WORLD.Barrier()
         print(f"{bcolors.OKGREEN}FINISHED{bcolors.ENDC}")
     
             
-    def read(self, pattern, variable: str, iterations: None, logging=False):
+    def read(self, pattern: str, variable: str, iterations: int, logging=False):
         
         patterns = {
             "bench_variable": self.__bench_variable,
